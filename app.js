ccf46901d49bc566751903966dcf4e367c14e8df
RichardGarciaP <68579638+RichardGarciaP@users.noreply.github.com>
2025-06-11 18:23:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGO_DB_URI;
+
+app.get('/api/posts', async (req, res) => {
+  let client;
+
+  try {
+    client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db('hdevops');
+    const posts = await db.collection('posts').find().toArray();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error('Error al consultar MongoDB:', error);
+    res
+      .status(500)
+      .json({ status: 500, message: 'Error al obtener los posts' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+});
+
+const PORT = process.env.NODEJS_PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Application running on port ', PORT);
+});
